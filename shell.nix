eee1144601e657eb041f273893b1d6af6e058436
Daniel Woffinden <daw@google.com>
2022-09-24 20:52:11 +0100

    Add `node_modules` to `nix-shell` with `node2nix`
    Plus manually patching `"nodejs-12_x"` out in `default.nix`
    
    TODO: updating it from an Action
    TODO: dream2nix?
    TODO: using it for deployment?

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-{ pg_version ? "10", pkgs ? import <nixpkgs> { } }:
+{ pg_version ? "10", pkgs ? import <nixpkgs> { }, system ? builtins.currentSystem }:
 let
   # update gemset.nix with bundix -l
   # e.g. nix-env --install bundix && bundix -l && nix-shell --run bundle exec jekyll serve
@@ -6,5 +6,11 @@ let
     name = "gems-for-some-project";
     gemdir = ./.;
   };
+  nodePackages = import ./default.nix { inherit pkgs system; };
 in
-pkgs.mkShell { packages = [ gems gems.wrappedRuby ]; }
+pkgs.mkShell {
+  packages = [ gems gems.wrappedRuby ];
+  shellHook = ''
+    ln -sfT ${nodePackages.nodeDependencies}/lib/node_modules node_modules
+  '';
+}
