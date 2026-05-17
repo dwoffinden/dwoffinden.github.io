91394d6854e87a488b1c3cbadb132875e5ede849
Daniel Woffinden <daw@hey.com>
2026-05-17 22:05:33 +0100

    eschew flake-utils

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
     git-hooks.url = "github:cachix/git-hooks.nix";
     git-hooks.inputs.nixpkgs.follows = "nixpkgs";
     flint.url = "github:notashelf/flint";
@@ -26,20 +25,65 @@
     {
       self,
       nixpkgs,
-      flake-utils,
       git-hooks,
       flint,
       zola-hallo,
       font-awesome,
       simple-icons,
+      ...
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        # TODO: read the toml for the theme name
-        # https://ilanjoselevich.com/blog/building-websites-using-nix-flakes-and-zola
-        themeName = "hallo";
+    let
+      forEachSystem = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+      pkgsFor = system: nixpkgs.legacyPackages.${system};
+
+      # TODO: read the toml for the theme name
+      # https://ilanjoselevich.com/blog/building-websites-using-nix-flakes-and-zola
+      themeName = "hallo";
+
+      copyTheme = ''
+        mkdir -p themes
+        rm -rf themes/${themeName}
+        cp -r --no-preserve=mode,ownership ${zola-hallo} themes/${themeName}
+        rm -rf themes/${themeName}/static/fontawesome
+      '';
+
+      copyIcons = ''
+        rm -rf fa-svgs
+        mkdir -p fa-svgs
+        cp --no-preserve=mode,ownership \
+          ${font-awesome}/svgs/{brands/{github,facebook,keybase,linkedin,stack-overflow},solid/{key,code,copy,check}}.svg \
+          ${simple-icons}/icons/matrix.svg \
+          fa-svgs/
+      '';
+    in
+    {
+      packages = forEachSystem (
+        system:
+        let
+          pkgs = pkgsFor system;
+        in
+        {
+          default = pkgs.stdenv.mkDerivation {
+            name = "dwoffinden-github-io";
+            src = ./.;
+            nativeBuildInputs = [ pkgs.zola ];
+
+            buildPhase = ''
+              ${copyTheme}
+              ${copyIcons}
+              zola build
+              rm public/{,images/}portrait.jpg
+              rmdir public/images
+            '';
+
+            installPhase = ''
+              cp -r public $out
+            '';
+          };
+        }
+      );
+
+      checks = forEachSystem (system: {
         pre-commit-check = git-hooks.lib.${system}.run {
           src = ./.;
           hooks = {
@@ -57,74 +101,47 @@
             };
           };
         };
+      });
 
-        copyTheme = ''
-          mkdir -p themes
-          rm -rf themes/${themeName}
-          cp -r --no-preserve=mode,ownership ${zola-hallo} themes/${themeName}
-          rm -rf themes/${themeName}/static/fontawesome
-        '';
-
-        copyIcons = ''
-          rm -rf fa-svgs
-          mkdir -p fa-svgs
-          cp --no-preserve=mode,ownership \
-            ${font-awesome}/svgs/{brands/{github,facebook,keybase,linkedin,stack-overflow},solid/{key,code,copy,check}}.svg \
-            ${simple-icons}/icons/matrix.svg \
-            fa-svgs/
-        '';
-      in
-      {
-        packages.default = pkgs.stdenv.mkDerivation {
-          name = "dwoffinden-github-io";
-          src = ./.;
-          nativeBuildInputs = [ pkgs.zola ];
-
-          buildPhase = ''
-            ${copyTheme}
-            ${copyIcons}
-            zola build
-            rm public/{,images/}portrait.jpg
-            rmdir public/images
+      formatter = forEachSystem (
+        system:
+        let
+          pkgs = pkgsFor system;
+          config = self.checks.${system}.pre-commit-check.config;
+          script = ''
+            ${pkgs.lib.getExe config.package} run --all-files --config ${config.configFile}
           '';
+        in
+        pkgs.writeShellScriptBin "pre-commit-run" script
+      );
 
-          installPhase = ''
-            cp -r public $out
-          '';
-        };
-
-        checks = {
-          inherit pre-commit-check;
-        };
-
-        formatter =
-          let
-            config = self.checks.${system}.pre-commit-check.config;
-            script = ''
-              ${pkgs.lib.getExe config.package} run --all-files --config ${config.configFile}
+      devShells = forEachSystem (
+        system:
+        let
+          pkgs = pkgsFor system;
+          pre-commit-check = self.checks.${system}.pre-commit-check;
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = pre-commit-check.enabledPackages;
+            packages = [
+              pkgs.zola
+              flint.packages.${system}.default
+            ];
+            shellHook = ''
+              ${pre-commit-check.shellHook}
+              if [[ ! -f themes/${themeName}/.source-path ]] || [[ "$(cat themes/${themeName}/.source-path)" != "${zola-hallo}" ]]; then
+                ${copyTheme}
+                echo "${zola-hallo}" > themes/${themeName}/.source-path
+              fi
+              if [[ ! -f fa-svgs/.source-path ]] || [[ "$(cat fa-svgs/.source-path)" != "$(echo -e "${font-awesome}\n${simple-icons}")" ]]; then
+                ${copyIcons}
+                echo -e "${font-awesome}\n${simple-icons}" > fa-svgs/.source-path
+              fi
+              echo "Zola dev shell loaded. Run 'zola serve' to start."
             '';
-          in
-          pkgs.writeShellScriptBin "pre-commit-run" script;
-
-        devShells.default = pkgs.mkShell {
-          buildInputs = pre-commit-check.enabledPackages;
-          packages = [
-            pkgs.zola
-            flint.packages.${system}.default
-          ];
-          shellHook = ''
-            ${pre-commit-check.shellHook}
-            if [[ ! -f themes/${themeName}/.source-path ]] || [[ "$(cat themes/${themeName}/.source-path)" != "${zola-hallo}" ]]; then
-              ${copyTheme}
-              echo "${zola-hallo}" > themes/${themeName}/.source-path
-            fi
-            if [[ ! -f fa-svgs/.source-path ]] || [[ "$(cat fa-svgs/.source-path)" != "$(echo -e "${font-awesome}\n${simple-icons}")" ]]; then
-              ${copyIcons}
-              echo -e "${font-awesome}\n${simple-icons}" > fa-svgs/.source-path
-            fi
-            echo "Zola dev shell loaded. Run 'zola serve' to start."
-          '';
-        };
-      }
-    );
+          };
+        }
+      );
+    };
 }
